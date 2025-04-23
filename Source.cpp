bc1217f54d5f61b9acacf6807b206f01c52bbcb7
Chlorid <roman11k@yandex.ru>
2025-04-23 17:28:09 +0300

    chlorid

--- Source.cpp
+++ Source.cpp
@@ -4,20 +4,20 @@
 using namespace std;
 int main() {
     setlocale(LC_ALL, "rus");
-
+Cout<<"Roma krytoy"<<;
     srand(time(0));
     int number = rand() % 99 + 1;
 
     if (number % 2 == 0) {
-        std::cout << "×åòíîå ÷èñëî." << std::endl;
+        std::cout << "Ã—Ã¥Ã²Ã­Ã®Ã¥ Ã·Ã¨Ã±Ã«Ã®." << std::endl;
     }
     else {
-        std::cout << "Íå÷åòíîå ÷èñëî." << std::endl;
+        std::cout << "ÃÃ¥Ã·Ã¥Ã²Ã­Ã®Ã¥ Ã·Ã¨Ã±Ã«Ã®." << std::endl;
     }
 
     if (number < 10) {
-        std::cout << "Îäíîçíà÷íîå" << std::endl;
+        std::cout << "ÃŽÃ¤Ã­Ã®Ã§Ã­Ã Ã·Ã­Ã®Ã¥" << std::endl;
     }
     else {
-        std::cout << "Äâóõçíà÷íîå" << std::endl;
+        std::cout << "Ã„Ã¢Ã³ÃµÃ§Ã­Ã Ã·Ã­Ã®Ã¥" << std::endl;
     }
